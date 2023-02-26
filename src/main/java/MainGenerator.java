6c01d61a25b146b6b20053fdecf0c4acec5707c8
Nikita Barsukov <bnkspbrus@gmail.com>
2023-02-27 00:30:17 +0300

    improvement

--- src/main/java/MainGenerator.java
+++ src/main/java/MainGenerator.java
@@ -3,16 +3,33 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
 public class MainGenerator {
     public static void main(String[] args) throws IOException {
-        CharStream input = CharStreams.fromFileName("Expr.in");
+        if (args.length != 2) {
+            System.err.println("2 arguments was expected. Given " + args.length);
+            System.exit(1);
+        }
+        CharStream input = null;
+        try {
+            input = CharStreams.fromFileName(args[0]);
+        } catch (InvalidPathException e) {
+            System.err.println("Invalid path to grammar was given");
+            System.exit(1);
+        }
         CommonTokenStream tokens = new CommonTokenStream(new GrammaticsLexer(input));
         GrammaticsParser parser = new GrammaticsParser(tokens);
         GrammaticsParser.GrammaticsContext ctx = parser.grammatics();
-//
-        Path genDir = Path.of("calculator/src/main/java");
+
+        Path genDir = null;
+        try {
+            genDir = Path.of(args[1]);
+        } catch (InvalidPathException e) {
+            System.err.println("Invalid destination path was given");
+            System.exit(1);
+        }
         Generator generator = new TableGenerator();
         generator.generate(ctx, genDir);
         Generator generator1 = new TokenGenerator();
